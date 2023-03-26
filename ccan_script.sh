a5426dfbde82c70a5aaf7a85ef96e225b6cfe19e
Madhushan B <30431222+bmsknight@users.noreply.github.com>
2023-03-25 23:29:36 -0400

    Create ccan_script.sh

---
+++ ccan_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -A def-kgroling
+#SBATCH --time 0-00:10:00   
+#SBARCH --n-task=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gpus=v100:1
+
+module load python
+module load scipy-stack
+source $HOME/myenv/bin/activate
+
+python main.py --epochs 5
